5e4d5b0dc702563af274f1359416effccc04f148
Kaiwalyani Gorde <116161221+kaiwalyani@users.noreply.github.com>
2024-03-25 18:59:19 +0530

    Update and rename Rollercoaster-height-approval.py to Rollercoaster-ride-approval.py

--- Rollercoaster-ride-approval.py
+++ Rollercoaster-ride-approval.py
@@ -2,5 +2,12 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 if height >= 120:
   print("You can ride the rollercoaster!")
+  age = input("What is your age?")
+  if int(age) < 12:
+    print("Please pay $5.")
+  elif int(age) <=18:
+    print("Please pay $7.")
+  else:
+    print("Please pay $12.")
 else:
   print("So sorry, you have to grow taller before you can ride.")
