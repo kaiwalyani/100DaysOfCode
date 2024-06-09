6dc0d9173136b45f3647c8c826f8257358f15cde
Kaiwalyani Gorde <116161221+kaiwalyani@users.noreply.github.com>
2024-06-09 17:07:25 +0530

    Create D19-Turtle-Race.py

---
+++ D19-Turtle-Race.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.setup(width = 500, height = 400)
+user_bet = screen.textinput(title="Make your bet", prompt = "Which turtle will win the race? Enter a color: ")
+print(user_bet)
+colors = ["red", "yellow","blue","green","purple","orange"]
+y_position = [-100,-60,-20,20,60,100]
+all_turtles = []
+
+for turtle_index in range(0,6):
+    new_turtle = Turtle(shape ="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
+
+is_race_on = False
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if user_bet == winning_color:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+        rand_dist = random.randint(0,10)
+        turtle.forward(rand_dist)
+is_race_on = False
+
+
+
+screen.exitonclick()
